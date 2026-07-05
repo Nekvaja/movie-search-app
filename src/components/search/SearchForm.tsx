d239d08b6c576c972d150a0d67b94329e27f55a2
Jana Nekvapilová <jana.nekva@gmail.com>
2026-07-05 14:12:29 +0200

    prevent search form submission on Enter

--- src/components/search/SearchForm.tsx
+++ src/components/search/SearchForm.tsx
@@ -55,7 +55,9 @@ export const SearchForm = () => {
 
     return (
       
-        <FormWrapper>
+        <FormWrapper
+            onSubmit={(e) => e.preventDefault()}
+        >
           <SearchIcon />
 
           <SearchInput
